23a6c7dc4e472ab23312d69c197d0679dbd0e66b
Andres.Calamar <andres15062010@gmail.com>
2024-09-03 18:49:03 -0600

    imageness y enlace sprint

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
+import img from './images/sier_b.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={img} className="App-logo" alt="logo" />
         <p>
           Hola Compañeros.
         </p>
@@ -14,11 +16,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://trello.com/invite/b/66c7abcb56e04fdad23ab712/ATTI2961928c18da95c6bf091b12b97e332aCB4271F2/sier"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Conoce los Sprits
         </a>
       </header>
     </div>
