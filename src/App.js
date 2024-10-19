a541bdc319bbe0f13c2e8c02687e661888066af6
Andres.Calamar <andres15062010@gmail.com>
2024-10-19 17:02:43 -0600

    ajustes home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import logo from './logo.svg';
-import img from './images/sier_b.png';
+// App.js
 import './App.css';
+import img from './images/sier_b.png'; // Ruta correcta de tu imagen
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <img src={img} className="App-logo" alt="logo" />
-        <p>
-          Hola Compañeros.
-        </p>
-        <p>
-        Actividad integradora. Sprint, el corazón de SCRUM
-        </p>
+    <div className="container">
+      <header className="header">
+        <img src={img} alt="Logo SIER" className="logo" />
+        <h1>Hola Compañeros</h1>
+        <p>Actividad integradora. Sprint, el corazón de SCRUM</p>
+      </header>
+
+      <main className="main-content">
         <a
-          className="App-link"
           href="https://trello.com/invite/b/66c7abcb56e04fdad23ab712/ATTI2961928c18da95c6bf091b12b97e332aCB4271F2/sier"
           target="_blank"
           rel="noopener noreferrer"
+          className="btn-link"
         >
-          Conoce los Sprits
+          Conoce los Sprints
         </a>
-      </header>
+      </main>
+
+      <footer className="footer">
+        <p>© 2024 INEGI - Todos los derechos reservados</p>
+      </footer>
     </div>
   );
 }
